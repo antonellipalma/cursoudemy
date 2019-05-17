eed8bdfdbe428b2885ce5c657acbf7e20b4f34ac
Marcelo Antonelli Palma <antonellipalma@gmail.com>
2019-05-17 13:40:28 -0300

    Produto e Associacao M->N

--- src/main/java/com/map/primeiroprojeto/PrimeiroProjetoApplication.java
+++ src/main/java/com/map/primeiroprojeto/PrimeiroProjetoApplication.java
@@ -1,17 +1,55 @@
 package com.map.primeiroprojeto;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.map.primeiroprojeto.domain.Categoria;
+import com.map.primeiroprojeto.domain.Produto;
+import com.map.primeiroprojeto.repositories.CategoriaRepository;
+import com.map.primeiroprojeto.repositories.ProdutoRepository;
 
 
 
 @SpringBootApplication
-public class PrimeiroProjetoApplication {
+public class PrimeiroProjetoApplication implements CommandLineRunner {
 
+	
+	@Autowired
+	CategoriaRepository repoCat;
+	
+	@Autowired
+	ProdutoRepository repoProd;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeiroProjetoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1= new Categoria(null,"Informatica");
+		Categoria cat2= new Categoria(null,"Escritorio"); 
+		
+		Produto p1= new Produto(null,"Computador",2000.00);
+		Produto p2= new Produto(null,"Impressora",800.00);
+		Produto p3= new Produto(null, "Mouse",80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p1.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		
+		repoCat.saveAll(Arrays.asList(cat1,cat2));
+		repoProd.saveAll(Arrays.asList(p1,p2,p3));
+		
+
+		
 	}
 
 }
--- src/main/java/com/map/primeiroprojeto/domain/Categoria.java
+++ src/main/java/com/map/primeiroprojeto/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.map.primeiroprojeto.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 
 
@@ -18,6 +21,9 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;	
 	private String nome;
+	
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos= new ArrayList<>();
 	
 	
 	public Categoria() {
@@ -69,5 +75,13 @@ public class Categoria implements Serializable{
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 }
