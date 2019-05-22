2f1d88ec8c02c312a0ee2c851c33dc2d6d0dc32a
Marcelo Antonelli Palma <antonellipalma@gmail.com>
2019-05-22 16:29:31 -0300

    Cliente, TipoCliente, Telefones e Endereco

--- src/main/java/com/map/primeiroprojeto/PrimeiroProjetoApplication.java
+++ src/main/java/com/map/primeiroprojeto/PrimeiroProjetoApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.map.primeiroprojeto.domain.Categoria;
 import com.map.primeiroprojeto.domain.Cidade;
+import com.map.primeiroprojeto.domain.Cliente;
+import com.map.primeiroprojeto.domain.Endereco;
 import com.map.primeiroprojeto.domain.Estado;
 import com.map.primeiroprojeto.domain.Produto;
+import com.map.primeiroprojeto.domain.enums.TipoCliente;
 import com.map.primeiroprojeto.repositories.CategoriaRepository;
 import com.map.primeiroprojeto.repositories.CidadeRepository;
+import com.map.primeiroprojeto.repositories.ClienteRepository;
+import com.map.primeiroprojeto.repositories.EnderecoRepository;
 import com.map.primeiroprojeto.repositories.EstadoRepository;
 import com.map.primeiroprojeto.repositories.ProdutoRepository;
 
@@ -33,6 +38,15 @@ public class PrimeiroProjetoApplication implements CommandLineRunner {
 	
 	@Autowired
 	EstadoRepository repoEst;
+	
+	@Autowired
+	ClienteRepository repoCli;
+	
+	@Autowired
+	EnderecoRepository repoEnd;
+	
+	
+	
 	
 	
 	public static void main(String[] args) {
@@ -70,8 +84,14 @@ public class PrimeiroProjetoApplication implements CommandLineRunner {
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2,c3));
 		
+	
+		Cliente cli1= new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323","9383893"));
 		
+		Endereco e1= new Endereco(null,"Rua Flores","300","Apto 203","Jardim","38220834",c1,cli1);
+		Endereco e2= new Endereco(null,"Avenida Matos","105","Sala800","Centro","38777012",c2,cli1);
 		
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
 		
 		
 		repoCat.saveAll(Arrays.asList(cat1,cat2));
@@ -79,7 +99,8 @@ public class PrimeiroProjetoApplication implements CommandLineRunner {
 		repoEst.saveAll(Arrays.asList(est1,est2));
 		repoCid.saveAll(Arrays.asList(c1,c2,c3));
 		
-
+		repoCli.saveAll(Arrays.asList(cli1));
+		repoEnd.saveAll(Arrays.asList(e1,e2));
 		
 	}
 
